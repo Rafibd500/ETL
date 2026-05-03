58fdd36097fe6d3316bed451dcf1b0f4e43dc5c8
Rafiqul Islam <rafibd500@gmail.com>
2026-05-03 22:26:33 +0600

    pharm easy completed

--- 1_API-data/Pharm_Easy/main.py
+++ 1_API-data/Pharm_Easy/main.py
@@ -7,12 +7,13 @@ prod_list = []
 category_response = requests.get('https://pharmeasy.in/api/home/fetchCategories')
 cat_res = category_response.json()
 categories=cat_res.get('data', {}).get('categories', [])
-for cat in categories:
-    cat_id = cat.get('id', '')
+for category in categories:
+    cat_id = category.get('id', '')
     i = 1
-    category_name = 
+    category_name = category.get('name')
     while(True):
         response = requests.get(f'https://pharmeasy.in/api/otc/getCategoryProducts?categoryId={cat_id}&page={i}')
+        print(f'https://pharmeasy.in/api/otc/getCategoryProducts?categoryId={cat_id}&page={i}')
         data = response.json()
         if(data.get('data') == {}):
             break
@@ -23,38 +24,46 @@ for cat in categories:
             # 4.1 Product-Level Fields
             name = product.get('name', 'Not Found')
             brand_name = product.get('manufacturer', 'Not Found')
-            price = 
-            discount_percent = 
-            description = 
-            img_url = 
-            # cat
-            sub_category = 
-            rating = 
-            num_of_review = 
-            availability = 
+            price = product.get('mrpDecimal', 'Not Found')
+            discount_percent = product.get('discountPercent', 'Not Found')
+            # img_url = product.get('images', [])[0]
+            # sub_category = 
+            
+            if not product.get('ratingDetails', {}):
+                rating = 'Not found'
+            else:
+                rating = product.get('ratingDetails', {}).get('value', -1)
+                rating = round(rating, 2)
+                num_of_review = product.get('ratingDetails', {}).get('count', -1)
+            
+            availability = 'In Stock' if product.get('isAvailable') == True else 'Out Of Stock' 
 
             # 4.2 Additional Fields
-            # SKU / Product ID
-            p_id = 
-            # Pack Size (e.g., 100ml, 10 tablets)
-            pack_size = 
-            # Ingredients / Composition
-            ingredients = 
-            # Usage Instructions
-            usage_instructions = 
-            # Manufacturer Name
-            manufacturer_name = 
-            # Expiry Information
-            expiry_info = 
-            # Delivery Time
-            deliver_time = 
+            # p_id = product.get('productId', 'Not Found')
+            # pack_size = 
+            # ingredients = 
+            # usage_instructions = 
+            # manufacturer_name = 
+            # expiry_info = 
+            # deliver_time = 
 
             # 4.3 Platform-Level Fields
-            # Category Name
-            category_name
-            # Total Number of Products
-            total_product = 
-            # Pagination (Page number)
-            page_number = i
+            # total_product = 
+            # page_number = i
             # Filters (Price range, brands, etc.)
+            dict = {
+                'Product Name':name,
+                'Brand':brand_name,
+                'Price' : price,
+                'Discount': discount_percent,
+                'Rating' : rating,
+                'Review' : num_of_review,
+                'Catergory' : category_name,
+                'Availability' : availability,
+            }
+            print(dict)
+            prod_list.append(dict)
         i+=1
+df = pd.DataFrame(prod_list)
+df.to_csv("pharm_easy.csv", index=False)
+print("=========Successfully Scraped=========")
